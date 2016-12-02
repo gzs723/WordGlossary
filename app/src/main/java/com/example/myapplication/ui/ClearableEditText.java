717fdc5e6787bf959a080c46f001710f70cbf4c7
916149034@qq.com <916149034@qq.com>
2016-12-02 13:13:17 +0800

    翻译commit

---
+++ app/src/main/java/com/example/myapplication/ui/ClearableEditText.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.ui;
+
+/**
+ * Created by 李思言 on 2016/11/29.
+ */
+
+public class ClearableEditText {
+}
---
+++ app/src/main/java/com/example/myapplication/ui/onTapped.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui;
+
+import android.content.Context;
+
+/**
+ * Created by 李思言 on 2016/11/29.
+ */
+
+public interface onTapped {
+
+    void jumpview(Boolean jumo);
+}
